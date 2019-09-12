e9ddf036e7a733f155279fa61e42be903c96ba0f
juancampos45 <55142610+juancampos45@users.noreply.github.com>
2019-09-12 10:26:27 -0400

    Add files via upload

---
+++ operadores2.py
@@ -0,0 +1,17 @@
+#  Propo8sito: continuando con los operadores aritmeticos
+
+# Entrada de datos
+a = float(input('ingrese el valor A: '))
+b = float(input('ingrese el valor B: '))
+c = float(input('ingrese el valor C: '))
+d = float(input('ingrese el valor D: '))
+
+# Proedimiento
+
+z = a + ((b)/(c - d))
+
+print('El resultado es: ',z)
+
+# Fin del programa
+
+input('pulse cualquier tecla para continuar...')# pausa
